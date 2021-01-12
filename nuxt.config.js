6cfc71158c4e5f6cf3ac877f485ab7d810684189
Jeroen Priem <jeroen@melonmedia.nl>
2021-01-12 09:57:14 +0100

    port & host

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  server: {
+    port: 8080, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'do-app',
