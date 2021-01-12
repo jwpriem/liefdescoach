ceaf531f73f13e80674b40b938f5f9b8516990f1
Jeroen Priem <jeroen@melonmedia.nl>
2021-01-12 19:31:39 +0100

    css tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,35 @@
 <template>
-  <div class="container mx-auto text-center min-h-screen flex justify-center items-center">
-    <svg class="w-32 h-32 inline-block text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
-    <h1 class="text-4xl font-black text-indigo-900">APP Platform on Digital Ocean</h1>
-  </div>
+  <section>
+    <header class="relative bg-white">
+      <div class="relative z-20 container mx-auto text-center min-h-screen flex justify-center items-center space-x-8">
+        <svg class="w-16 h-16 md:w-32 md:h-32 inline-block text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" /></svg>
+        <h1 class="text-2xl md:text-6xl font-black text-white">
+          APP Platform on Digital Ocean
+        </h1>
+      </div>
+      <div class="absolute right-0 bottom-0 h-full w-screen bg-black z-10 bg-opacity-25" />
+      <div class="absolute left-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10" />
+      <div class="absolute left-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 ml-10 md:ml-20" />
+      <div class="absolute left-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mb-10 md:mb-20 bg-opacity-75" />
+      <div class="absolute left-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 ml-20 md:ml-40 bg-opacity-50" />
+      <div class="absolute left-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mb-20 md:mb-40 bg-opacity-25" />
+      <div class="absolute left-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mb-10 md:mb-20 ml-10 md:ml-20 bg-opacity-50" />
+
+      <div class="absolute right-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10" />
+      <div class="absolute right-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mr-10 md:mr-20 bg-opacity-75" />
+      <div class="absolute right-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mb-10 md:mb-20" />
+      <div class="absolute right-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mr-20 md:mr-40 bg-opacity-25" />
+      <div class="absolute right-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mb-20 md:mb-40 bg-opacity-25" />
+      <div class="absolute right-0 bottom-0 h-10 md:h-20 w-10 md:w-20 bg-white z-10 mb-10 md:mb-20 mr-10 md:mr-20 bg-opacity-50" />
+
+      <img class="absolute right-0 top-0 z-0 h-full w-screen object-cover" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80">
+    </header>
+    <div class="h-screen text-center py-24 md:py-48 w-10/12 md:w-1/2 mx-auto">
+      <p class="text-base leading-snug md:text-xl md:leading-loose text-gray-500">
+        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
