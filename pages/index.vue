f0c96dbfa3d4c9c38b124809211024176c18c7d8
Jeroen Priem <jeroen@melonmedia.nl>
2021-01-12 11:21:32 +0100

    Thumbs up

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="container mx-auto text-center">
-    <h1 class="text-6xl font-black">APP Platform on Digital Ocean</h1>
+  <div class="container mx-auto text-center min-h-screen flex justify-center items-center">
+    <svg class="w-32 h-32 inline-block text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
+    <h1 class="text-4xl font-black text-indigo-900">APP Platform on Digital Ocean</h1>
   </div>
 </template>
 
