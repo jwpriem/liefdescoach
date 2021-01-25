365924a0be000d35245c68b7b43d527c7f52fadd
Jeroen Priem <jeroen@melonmedia.nl>
2021-01-25 10:14:49 +0100

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
       </div>
       <div class="h-full flex justify-center items-center md:p-16 p-8">
         <h1 class="md:text-4xl text-2xl text-gray-500">
-          Wil je vertrouwen in je relatie? Of gewoon een zetje in de rug met online daten? De <span class="text-pink-300">Liefdescoach</span> helpt je verder
+          Wil je meer vertrouwen in je relatie? Of gewoon een zetje in de rug met online daten? De <span class="text-pink-300">Liefdescoach</span> helpt je verder
         </h1>
       </div>
       <img class="md:h-screen h-64 w-full object-cover" src="https://images.unsplash.com/photo-1522033467113-b8db23657b96?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
