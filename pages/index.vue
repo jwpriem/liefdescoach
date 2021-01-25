82f8a846398de6d7ae9445e382d19212dba2dd6e
Jeroen Priem <jeroen@melonmedia.nl>
2021-01-25 10:02:57 +0100

    Logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
   <section>
-    <header class="grid md:grid-cols-2 grid-cols-1">
-      <div class="flex justify-center items-center md:p-16 p-8">
-        <h1 class="md:text-6xl text-2xl text-gray-600">
-          <span class="text-pink-300">Liefdescoach</span> voor jezelf of samen
+    <header class="grid md:grid-cols-2 grid-cols-1 relative">
+      <div class="absolute top-0 left-0 m-6">
+        <div class="w-full text-pink-300 flex justify-start items-center p-8">
+          <span class="text-gray-700 text-3xl font-black">
+            Liefdescoach
+          </span>
+          <svg class="w-12 h-12 inline-block stroke-current" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+        </div>
+      </div>
+      <div class="h-full flex justify-center items-center md:p-16 p-8">
+        <h1 class="md:text-4xl text-2xl text-gray-500">
+          Wil je vertrouwen in je relatie? Of gewoon een zetje in de rug met online daten? De <span class="text-pink-300">Liefdescoach</span> helpt je verder
         </h1>
       </div>
       <img class="md:h-screen h-64 w-full object-cover" src="https://images.unsplash.com/photo-1522033467113-b8db23657b96?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80">
