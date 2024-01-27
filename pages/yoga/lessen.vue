c0ec5d3fd4857837529bbd5283b3e0f3cad94a50
jwpriem <jwpriem@gmail.com>
2024-01-27 14:35:54 +0100

    fix button

--- pages/yoga/lessen.vue
+++ pages/yoga/lessen.vue
@@ -13,7 +13,7 @@
           </svg>
           {{ formatDateInDutch(lesson.date) }} ({{ lesson.spots }} plekken)
           <button :disabled="checkBooking(lesson.$id)" class="button emerald button-small"
-                  :class="checkBooking(lesson.$id) ? 'disabled' : ''" @click="book(lesson)" v-if="loggedInUser">
+                  :class="checkBooking(lesson.$id) ? 'disabled' : ''" @click="book(lesson)" v-if="loggedInUser && !checkBooking(lesson.$id)">
                   <svg v-if="isLoading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white inline-block"
                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle
