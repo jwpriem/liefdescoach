dcb685ca247f7300244e9aa8fc1956722b5f4e30
jwpriem <jwpriem@gmail.com>
2024-03-31 18:26:26 +0200

    margin bottom

--- pages/yoga/lessen.vue
+++ pages/yoga/lessen.vue
@@ -62,7 +62,7 @@ async function login() {
       </h1>
       <div class="intro">
         <div v-for="lesson in $rav.upcomingLessons(lessons)" :key="lesson.$id" v-if="lessons.length"
-             class="flex items-center gap-x-3">
+             class="flex items-center gap-x-3 mb-3">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                stroke="currentColor" class="w-6 h-6 mr-1 inline-block stroke-current text-emerald-700">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
