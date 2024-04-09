9780073d2624a4225f4fe46d24be0ce55d8d5356
jwpriem <jwpriem@gmail.com>
2024-04-09 20:12:00 +0200

    Get only future lessons

--- server/api/lessons.get.ts
+++ server/api/lessons.get.ts
@@ -5,19 +5,21 @@ import { useRuntimeConfig } from '#imports';
 export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
     const client = new Client();
-    
+
     client
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
     .setProject(config.public.project) // Your project ID
     .setKey(config.appwriteKey) // Your secret API key
-    
+
     const databases = new Databases(client);
+    const fromDate = new Date()
     
     const res = await databases.listDocuments(
         config.public.database,
         'lessons',
         [
-            Query.orderAsc("date")
+            Query.orderAsc("date"),
+            Query.greaterThanEqual("date", fromDate.toISOString())
         ]
     );
     
