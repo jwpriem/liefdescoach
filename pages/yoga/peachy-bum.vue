23f43f6fb5d154ed38275ad37124ecade29af255
jwpriem <jwpriem@gmail.com>
2024-04-09 20:47:34 +0200

    peachy-bum text updates and photos

--- pages/yoga/peachy-bum.vue
+++ pages/yoga/peachy-bum.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 const title = ref('Yoga Ravennah | Peachy Bum');
 const description = ref('Wil je graag een keer een iets actievere les? Bij mooi weer van april tot september geef ik een peachy bum les op het strand van Rotterdam Nesselande');
-const ogImage = ref('https://www.ravennah.com/ravennah-social.jpg');
-const pageUrl = ref('https://www.ravennah.com/yoga/over');
+const ogImage = ref('https://www.ravennah.com/peachy-bum-social.webp');
+const pageUrl = ref('https://www.ravennah.com/yoga/peachy-bum');
 
 definePageMeta({
     layout: 'yoga'
@@ -29,7 +29,7 @@ useHead({
 
 <template>
   <div>
-      <Header image="/warmingup.webp" alignment="object-middle">
+      <Header image="/peachy-bum.webp" alignment="object-middle">
         <div class="flex justify-start items-center gap-x-6">
           <h1 class="text-3xl md:text-6xl uppercase font-black">
               <span class="emerald-underline">Peachy Bum</span
@@ -41,7 +41,7 @@ useHead({
         Wie graag iets actievers doet dan yoga en het leuk vind om buiten te sporten geef ik in de zomermaanden bij goed weer van april tot september Peachy Bum lessen! 
           </p>
           <p class="intro">
-          De Peachy Bum is een andere benaming voor mooie getrainde billen. De musculus gluteus maximus oftewel grote bilspier is de grootste van de drie bilspieren en bepaalt een groot deel van de vorm van de billen. Tijdens de Peachy Bum les zullen we allerlei leuke oefeningen doen om deze spier optimaal te trainen en zo te werken aan de (nog mooiere) billen!
+          De Peachy Bum is een andere benaming voor mooie getrainde billen. De musculus gluteus maximus oftewel grote bilspier is de grootste van de drie bilspieren en bepaalt een groot deel van de vorm van de billen. Tijdens de Peachy Bum les zullen we allerlei leuke oefeningen doen om deze spier optimaal te trainen en zo te werken aan de (nog mooiere) billen! Ook trainen we natuurlijk je core voor optimale stabiliteit en houding, wat natuurlijk weer van pas komt bij yoga.
           </p>
           <div
               class="flex justify-start items-center gap-x-3"
