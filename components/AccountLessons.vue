164ac3434b1689f8a1ed728dada1b966ab4fd94a
jwpriem <jwpriem@gmail.com>
2026-02-01 15:04:43 +0100

    Login lessons gastles

--- components/AccountLessons.vue
+++ components/AccountLessons.vue
@@ -79,7 +79,7 @@ async function createNewLesson() {
 }
 
 async function removeBooking(booking, lesson) {
-		await store.setOnBehalfOf(JSON.parse(state.addBookingUser))
+  await store.setOnBehalfOf(JSON.parse(state.addBookingUser))
   await store.cancelBooking(booking, lesson)
 }
 
@@ -123,88 +123,82 @@ const computedStudents = computed(() => {
 
 <template>
   <div v-if="isAdmin && lessons && students">
-   <div class="w-full">
-    <div class="md:flex justify-between items-center">
-      <h2 class="text-2xl md:text-4xl uppercase font-black">
-       <span class="emerald-underline text-emerald-900">Lessen</span><span class="text-emerald-700">.</span>
-     </h2>
-     <div class="flex flex-wrap items-center gap-x-3">
-       <UButton color="primary" variant="solid" @click="state.createLesson = !state.createLesson">Voeg les toe</UButton>
-       <UButton color="primary" variant="solid" @click="state.bookForUser = !state.bookForUser">Maak boeking voor gebruiker</UButton>
-      <UButton color="primary" variant="outline" to="/yoga/archief">Archief</UButton>
-     </div>
-    </div>
-    <div class="grid grid-cols-1 md:grid-cols-4 mt-8 gap-3">
-      <div v-for="lesson in lessons" index="lesson.$id" class="p-4 bg-gray-800 rounded flex flex-col gap-y-3">
-        <div>
-          <sup class="text-emerald-500">Les</sup>
-          <span class="block -mt-2 capitalize">{{ $rav.checkLessonType(lesson.type) }}</span>
+    <div class="w-full">
+      <div class="md:flex justify-between items-center">
+        <h2 class="text-2xl md:text-4xl uppercase font-black">
+          <span class="emerald-underline text-emerald-900">Lessen</span><span class="text-emerald-700">.</span>
+        </h2>
+        <div class="flex flex-wrap items-center gap-x-3">
+          <UButton color="primary" variant="solid" @click="state.createLesson = !state.createLesson">Voeg les toe
+          </UButton>
+          <UButton color="primary" variant="solid" @click="state.bookForUser = !state.bookForUser">Maak boeking voor
+            gebruiker</UButton>
+          <UButton color="primary" variant="outline" to="/yoga/archief">Archief</UButton>
         </div>
-        <div>
-          <sup class="text-emerald-500">Datum</sup>
-          <span class="block -mt-2">{{ $rav.formatDateInDutch(lesson.date, true) }}</span>
-        </div>
-        <div>
-          <sup class="text-emerald-500">Boekingen ( {{ lesson.bookings?.length || 0 }}/9 )</sup>
-          <span class="block -mt-2">
-            <span v-for="booking in sortStudents(lesson.bookings || [])" index="booking.$id" class="block">
-              {{ booking.students.name }}
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cursor-pointer w-5 h-5 ml-3 inline-block text-red-300" @click="removeBooking(booking, lesson)">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-              </svg>
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-4 mt-8 gap-3">
+        <div v-for="lesson in lessons" index="lesson.$id" class="p-4 bg-gray-800 rounded flex flex-col gap-y-3">
+          <div>
+            <sup class="text-emerald-500">Les</sup>
+            <span class="block -mt-2 capitalize" v-if="lesson.teacher == null">{{ $rav.checkLessonType(lesson.type)
+              }}</span><span v-else class="block -mt-2 text-orange-900 font-bold">Yin-Yang Yoga door gastdocent {{
+                lesson.teacher }}</span>
+          </div>
+          <div>
+            <sup class="text-emerald-500">Datum</sup>
+            <span class="block -mt-2">{{ $rav.formatDateInDutch(lesson.date, true) }}</span>
+          </div>
+          <div>
+            <sup class="text-emerald-500">Boekingen ( {{ lesson.bookings?.length || 0 }}/9 )</sup>
+            <span class="block -mt-2">
+              <span v-for="booking in sortStudents(lesson.bookings || [])" index="booking.$id" class="block">
+                {{ booking.students.name }}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                  stroke="currentColor" class="cursor-pointer w-5 h-5 ml-3 inline-block text-red-300"
+                  @click="removeBooking(booking, lesson)">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                </svg>
+              </span>
+              <span v-if="!lesson.bookings?.length">Geen boekingen</span>
             </span>
-            <span v-if="!lesson.bookings?.length">Geen boekingen</span>
-          </span>
+          </div>
         </div>
       </div>
     </div>
-   </div>
 
-   <!--Book for user-->
-    <div v-if="state.bookForUser && lessons.length && students.length" class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+    <!--Book for user-->
+    <div v-if="state.bookForUser && lessons.length && students.length"
+      class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
       <div class="w-full max-height-75 overflow-y-scroll sm:w-2/3 md:w-1/2 bg-gray-800 p-6 rounded-lg shadow-lg">
         <!-- Your form content goes here -->
         <div class="w-full flex flex-col gap-y-5">
           <h2 class="text-2xl md:text-4xl uppercase font-black">
-              <span class="emerald-underline text-emerald-900"
-              >Voeg boeking toe</span
-              ><span class="text-emerald-700">.</span>
+            <span class="emerald-underline text-emerald-900">Voeg boeking toe</span><span
+              class="text-emerald-700">.</span>
           </h2>
-          <USelect
-            icon="i-heroicons-academic-cap-20-solid"
-            size="md"
-            color="primary"
-            variant="outline"
-            v-model="state.addBookingLesson"
-            :options="computedLessons"
-            />
+          <USelect icon="i-heroicons-academic-cap-20-solid" size="md" color="primary" variant="outline"
+            v-model="state.addBookingLesson" :options="computedLessons" />
 
-          <USelect
-            v-if="state.addBookingLesson"
-            icon="i-heroicons-user-20-solid"
-            size="md"
-            color="primary"
-            variant="outline"
-            v-model="state.addBookingUser"
-            :options="computedStudents"
-            />
+          <USelect v-if="state.addBookingLesson" icon="i-heroicons-user-20-solid" size="md" color="primary"
+            variant="outline" v-model="state.addBookingUser" :options="computedStudents" />
 
           <div class="flex flex-wrap gap-x-3">
-            <UButton color="primary" variant="solid" @click="book()" :disabled="!state.addBookingUser && !state.addBookingLesson">Voeg toe</UButton>
+            <UButton color="primary" variant="solid" @click="book()"
+              :disabled="!state.addBookingUser && !state.addBookingLesson">Voeg toe</UButton>
             <UButton color="primary" variant="outline" @click="cancel()">Annuleer</UButton>
           </div>
         </div>
       </div>
     </div>
     <!--Create lesson-->
-    <div v-if="state.createLesson && isAdmin" class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+    <div v-if="state.createLesson && isAdmin"
+      class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
       <div class="w-full max-height-75 overflow-y-scroll sm:w-2/3 md:w-1/2 bg-gray-800 p-6 rounded-lg shadow-lg">
         <!-- Your form content goes here -->
         <div class="w-full flex flex-col gap-y-5">
           <h2 class="text-2xl md:text-4xl uppercase font-black">
-            <span class="emerald-underline text-emerald-900"
-              >Voeg les toe</span
-              ><span class="text-emerald-700">.</span>
+            <span class="emerald-underline text-emerald-900">Voeg les toe</span><span class="text-emerald-700">.</span>
           </h2>
 
           <UPopover :popper="{ placement: 'bottom-start' }">
@@ -216,34 +210,16 @@ const computedStudents = computed(() => {
           </UPopover>
 
           <div class="flex justify-start items-center gap-x-3">
-          <USelect
-          icon="i-heroicons-clock-20-solid"
-          size="md"
-          color="primary"
-          variant="outline"
-          v-model="state.createLessonHours"
-          :options="state.hours"
-          />
+            <USelect icon="i-heroicons-clock-20-solid" size="md" color="primary" variant="outline"
+              v-model="state.createLessonHours" :options="state.hours" />
 
 
-          <USelect
-          icon="i-heroicons-clock-20-solid"
-          size="md"
-          color="primary"
-          variant="outline"
-          v-model="state.createLessonMinutes"
-          :options="state.minutes"
-          />
+            <USelect icon="i-heroicons-clock-20-solid" size="md" color="primary" variant="outline"
+              v-model="state.createLessonMinutes" :options="state.minutes" />
           </div>
 
-          <USelect
-            icon="i-heroicons-academic-cap-20-solid"
-            size="md"
-            color="primary"
-            variant="outline"
-            v-model="state.createLessonType"
-            :options="state.types"
-          />
+          <USelect icon="i-heroicons-academic-cap-20-solid" size="md" color="primary" variant="outline"
+            v-model="state.createLessonType" :options="state.types" />
 
           <div class="flex flex-wrap gap-x-3">
             <UButton color="primary" variant="solid" @click="createNewLesson()">Voeg toe</UButton>
